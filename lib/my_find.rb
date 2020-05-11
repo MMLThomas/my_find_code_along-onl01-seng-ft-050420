34e4d1787c2609aa4a0550e1cba7c8f83e1e745f
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 02:53:42 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  if new_collection.find 
 end
